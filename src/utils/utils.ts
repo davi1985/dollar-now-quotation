507b31c6289196036b96e8cfb4f58aea54483195
Davi Silva <davi.psilva@midway.com.br>
2024-06-14 09:45:31 -0300

    add implicit return

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,10 @@
-export const customFormatCurrency = (currency: string, value: number) => {
-  return new Intl.NumberFormat(currency, {
+export const customFormatCurrency = (currency: string, value: number) =>
+  new Intl.NumberFormat(currency, {
     style: 'currency',
     currency: currency.toUpperCase(),
   }).format(value);
-};
 
-export const currenyBRL = (value: number) =>
+export const currentBRL = (value: number) =>
   new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
