9c0df7816f2493cba0297fe75928cdcb6027f8b0
Davi Silva <davi.psilva@midway.com.br>
2024-06-14 09:44:35 -0300

    add toast container in main file

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { App } from './App';
 import { GlobalStyle } from './styles/global';
 
 import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
     <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root'),
