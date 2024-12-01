09128555cc2bef5a0d4c96566f41d5dff0d3df0e
Davi Silva <davisilvaphoto@gmail.com>
2024-11-30 21:13:57 -0300

    update styles

--- src/components/Cards/styles.ts
+++ src/components/Cards/styles.ts
@@ -4,4 +4,10 @@ export const Container = styled.div`
   display: flex;
   gap: 2rem;
   flex: 1;
+
+  @media (max-width: 768px) {
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  }
 `;
