a75c9d254d4e9a599ad2e3083621e3a5b5155e59
Davi Silva <davisilvaphoto@gmail.com>
2024-11-30 21:03:46 -0300

    add vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
-// File: vitest.config.ts
-import { configDefaults, defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'node:path';
 
+// https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   test: {
-    ...configDefaults,
-    environment: 'jsdom',
-    globals: true,
+    transformMode: {
+      web: [/\.tsx$/],
+    },
     setupFiles: './setup.ts',
-    css: false,
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
   },
 });
